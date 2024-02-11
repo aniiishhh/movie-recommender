12d4eddc8b5793e8304035064e7c01cbb7e5fb9f
aniiishhh <anishk@Anishs-MacBook-Air.local>
2024-02-10 19:46:39 -0700

    add cover pic

--- home.py
+++ home.py
@@ -7,7 +7,7 @@ def home():
     st.write(
         "Welcome to Movie Recommender, your ultimate destination for discovering new movies and exploring vast collections of cinematic treasures. This app utilizes cutting-edge technology to bring you personalized movie recommendations and advanced filtering options to cater to your unique tastes."
     )
-
+    st.write("")
     st.image("home_collage.jpg")
 
     # # Recommendations Section
